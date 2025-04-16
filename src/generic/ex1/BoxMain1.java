34c6f20f457e2b539c8e7c019d199f4cd929b1d0
zwoong <zwoong6867@gmail.com>
2025-04-16 23:04:22 +0900

    ✨ BoxMain1

---
+++ src/generic/ex1/BoxMain1.java
@@ -0,0 +1,17 @@
+package generic.ex1;
+
+public class BoxMain1 {
+
+  public static void main(String[] args) {
+    IntegerBox integerBox = new IntegerBox();
+    integerBox.setValue(10);
+    Integer integer = integerBox.getValue();
+    System.out.println("integer = " + integer);
+
+    StringBox stringBox = new StringBox();
+    stringBox.setValue("hello");
+    String str = stringBox.getValue();
+    System.out.println("str = " + str);
+
+  }
+}
