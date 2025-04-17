95e55912144d2f8582e5728418e8971b02570eeb
zwoong <zwoong6867@gmail.com>
2025-04-17 22:58:59 +0900

    ✨ ContainerTest

---
+++ src/generic/test/ex1/Container.java
@@ -0,0 +1,18 @@
+package generic.test.ex1;
+
+public class Container<T> {
+  private T item;
+
+  public T getItem() {
+    return item;
+  }
+
+  public void setItem(T item) {
+    this.item = item;
+  }
+
+  public boolean isEmpty() {
+    return item == null;
+  }
+
+}
