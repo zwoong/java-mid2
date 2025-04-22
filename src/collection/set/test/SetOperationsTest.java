1248e98170f650dca7341c9cdb4c211a7a5a6d4a
zwoong <zwoong6867@gmail.com>
2025-04-22 15:17:41 +0900

    ✨  RectangleTest

---
+++ src/collection/set/test/SetOperationsTest.java
@@ -0,0 +1,28 @@
+package collection.set.test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class SetOperationsTest {
+
+  public static void main(String[] args) {
+    Set<Integer> set1 = new HashSet<>(List.of(1, 2, 3, 4, 5));
+    Set<Integer> set2 = new HashSet<>(List.of(3, 4, 5, 6, 7));
+
+    // 합집합
+    Set<Integer> union = new HashSet<>(set1);
+    union.addAll(set2);
+    System.out.println("합집합: " + union);
+
+    // 교집합
+    Set<Integer> intersection = new HashSet<>(set1);
+    intersection.retainAll(set2);
+    System.out.println("교집합: " + intersection);
+
+    // 차집합
+    Set<Integer> difference = new HashSet<>(set1);
+    difference.removeAll(set2);
+    System.out.println("차집합: " + difference);
+  }
+}
