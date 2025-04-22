5a8e5edf6bc3ea8e5b8cf39d2ec2b0016de22e57
zwoong <zwoong6867@gmail.com>
2025-04-22 17:35:13 +0900

    ✨  MapMain3

---
+++ src/collection/map/MapMain3.java
@@ -0,0 +1,25 @@
+package collection.map;
+
+import java.util.HashMap;
+
+public class MapMain3 {
+
+  public static void main(String[] args) {
+    HashMap<String, Integer> studentMap = new HashMap<>();
+
+    // 학생 성적 데이터 추가
+    studentMap.put("studentA", 50);
+    System.out.println("studentMap = " + studentMap);
+
+    // 학생이 없는 경우에만 추가1
+    if (!studentMap.containsKey("studentA")) {
+      studentMap.put("studentA", 100);
+    }
+    System.out.println("studentMap = " + studentMap);
+
+    // 학생이 없는 경우에만 추가1
+    studentMap.putIfAbsent("studentA", 100);
+    studentMap.putIfAbsent("studentB", 100);
+    System.out.println("studentMap = " + studentMap);
+  }
+}
