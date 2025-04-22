ee65c560e05c20c7cfe7f07eb475b056720935e9
zwoong <zwoong6867@gmail.com>
2025-04-22 12:30:32 +0900

    ✨  MemberOnlyHash

---
+++ src/collection/set/member/MemberOnlyHash.java
@@ -0,0 +1,27 @@
+package collection.set.member;
+
+import java.util.Objects;
+
+public class MemberOnlyHash {
+  private String id;
+
+  public MemberOnlyHash(String id) {
+    this.id = id;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  @Override
+  public String toString() {
+    return "MemberOnlyHash{" +
+        "id='" + id + '\'' +
+        '}';
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id);
+  }
+}
